2c18a5b70f2ea59079133eaad945bd3bff175077
Özgür KURUL <kurul6767@gmail.com>
2023-11-26 17:54:35 +0300

    mainpage fix

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -3,12 +3,7 @@ import React, { useEffect, useState } from "react";
 //Import Components
 import Navbar from "./Navbar/Navbar"
 import Section from "./HeroSection/Section"
-import CardsMini from "./HeroSection/cards-mini"
-import AboutUs from "./AboutUs/about-us"
 import Features from "./Features/features"
-import RoadMap from "./RoadMap/road-map"
-import Blog from "./Blog/blog"
-import FAQs from "./Faqs/FAQs"
 import Footer from "./Footer/footer"
 
 const Mainpage = () => {
