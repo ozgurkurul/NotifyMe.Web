fecdb4911265097ef0bf5bc74ac2c4adba2fe744
Özgür KURUL <kurul6767@gmail.com>
2024-07-28 20:38:41 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173, // This is the port which we will use in docker
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
+  },
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment'
